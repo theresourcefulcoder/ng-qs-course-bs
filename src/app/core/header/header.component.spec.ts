568a17ba69b42e9a4769c15aaf1b1470bbdb3261
theresourcefulcoder <contact@theresourcefulcoder.com>
2019-04-05 20:02:01 -0400

    Added JWT support, updated header component and shared module

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [ NgbModule ]
     })
     .compileComponents();
   }));
